94a67c5f0346d0f7c8c510e45912c1e5bb1a231a
Scott Kerlagon <scott.kerlagon@gmail.com>
2019-10-04 13:20:29 -0700

    Stretch2 Finished. The process continues and allows for multiple runs until program is terminated.

--- ChallengeCalculator/Program.cs
+++ ChallengeCalculator/Program.cs
@@ -6,25 +6,27 @@ namespace ChallengeCalculator
     {
         static void Main()
         {
-            Calculator calc = new Calculator();
-            Console.WriteLine("Enter numbers with ',' or '\\n' between them to be added together, x to finish.");
 
             //Enter on windows is expressed as \r\n where the requirents state \n for new line. Since there is no good way I can find to accept a \n for newline
             // as console input I will use the enter key's \r\n to do the same. Linux line endings are \n and windows tends to use \r\n.
             //Another possible way would be to unescape \\n from the input from console and proceed from there.
 
-            string input = string.Empty;
             while (true)
             {
-                string line = Console.ReadLine();
-                if (line.ToLower() == "x")
-                    break;
-                input += line + '\n'; // Adding newline from enter key.
+                Calculator calc = new Calculator();
+                Console.WriteLine("Enter numbers with ',' or '\\n' between them to be added together, x to finish and start over.");
+
+                string input = string.Empty;
+                while (true)
+                {
+                    string line = Console.ReadLine();
+                    if (line.ToLower() == "x")
+                        break;
+                    input += line + '\n'; // Adding newline from enter key.
+                }
+                var result = calc.ProcessInput(input);
+                Console.Write(result.Answer + Environment.NewLine + result.Formula + Environment.NewLine);
             }
-            var result = calc.ProcessInput(input);
-            Console.Write(result.Answer + Environment.NewLine + result.Formula);
-            Console.Write($"{Environment.NewLine}Press any key to end.{Environment.NewLine}");
-            Console.ReadKey();
         }
     }
 }
